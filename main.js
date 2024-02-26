4acc0611daa8b1da24a7c682f469f4ed869a0246
RheaP911 <rhealynpanganiban20@gmail.com>
2024-02-26 17:00:23 +0800

    Make the navigation  works

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+//Ripple Effect on Hire me Button
 const buttons = document.getElementsByClassName('ripple');
 buttons.forEach(btn => {
     btn.addEventListener('click', function(e) {
@@ -14,3 +15,20 @@ buttons.forEach(btn => {
         }, 1000);
     })
 })
+
+
+//Navigation Hover
+document.addEventListener("DOMContentLoaded", function() {
+    const navButtons = document.querySelectorAll('.nav-button');
+
+    navButtons.forEach(function(button) {
+        button.addEventListener('click', function() {
+
+            navButtons.forEach(function(btn) {
+                btn.classList.remove('active');
+            });
+
+            this.classList.add('active');
+        });
+    });
+});
