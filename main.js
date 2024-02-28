ee360e65014a258be6b3170a828da6162e0b1d28
RheaP911 <rhealynpanganiban20@gmail.com>
2024-02-28 17:19:26 +0800

    Edited Hire Me Button

--- main.js
+++ main.js
@@ -1,20 +1,16 @@
 //Ripple Effect on Hire me Button
 const buttons = document.querySelector('ripple');
-console.log(typeof buttons);
-buttons.forEach(btn => {
-    btn.addEventListener('click', function(e) {
-        console.log(e);
-        let x = e.clientX - e.target.offsetLeft;
-        let y = e.clientY - e.target.offsetTop;
+btn.addEventListener('click', function(e) {
+    let x = e.clientX - e.target.offsetLeft;
+    let y = e.clientY - e.target.offsetTop;
 
-        let ripples = document.createElement('span');
-        ripples.style.left = x + 'px';
-        ripples.style.top = y + 'px';
-        this.appendChild(ripples);
+    let ripples = document.createElement('span');
+    ripples.style.left = x + 'px';
+    ripples.style.top = y + 'px';
+    this.appendChild(ripples);
 
-        setTimeout(() => {
-            ripples.remove()
-        }, 1000);
-    })
+    setTimeout(() => {
+        ripples.remove()
+    }, 1000);
 })
 
